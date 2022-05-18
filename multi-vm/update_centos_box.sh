e8c3d13df7eba3f81678d6dd3ed278fa86da2269
Paul Joseph Devlin <devunderslash@users.noreply.github.com>
2022-05-18 08:49:16 +0000

    Adding new boxes

---
+++ multi-vm/update_centos_box.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd /etc/yum.repos.d/
+
+sudo sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
+sudo sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
+
+yum update -y
